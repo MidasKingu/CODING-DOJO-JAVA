750c954ac70d20c0f9960d0e3281caae34d7c3f2
Jorge Saavedra <jorge.saavedra@trebol-it.com>
2023-03-29 01:14:27 -0300

    ejercicios

--- Fundamentals/basic-exercises/src/BasicJava.java
+++ Fundamentals/basic-exercises/src/BasicJava.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class BasicJava {
     void printFrom1To255() {
@@ -26,6 +27,65 @@ public class BasicJava {
             System.out.println(array[i]);
         }
     }
+    void max(int[] array) {
+        int max = array[0];
+        for(int i = 1 ; i < array.length; i++) {
+            if(max < array[i]){
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+    }
 
+    void average(int[] array) {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        System.out.println(sum/array.length);
+    }
+
+    void greaterThanY(int[] array, int y) {
+        int count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if( array[i] > y) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    void valuesElevated(int[] array) {
+
+        for (int i = 0 ; i < array.length; i++){
+            array[i] = array[i] * array[i];
+        }
+        System.out.printf(Arrays.toString(array));
+    }
+    void replaceNegatives(int[] array) {
+
+        for (int i = 0 ; i < array.length; i++){
+            if(array[i] < 0) {
+                array[i] = 0;
+            }
+        }
+        System.out.printf(Arrays.toString(array));
+    }
+    void minMaxAndAvg(int[] array) {
+        int max = array[0];
+        int min = array[0];
+        int sum = 0;
+        int[] values = {max,min,sum/array.length};
+        for(int i = 1 ; i < array.length; i++) {
+            sum += array[i];
+            if(max < array[i]){
+                max = array[i];
+            }
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.println(Arrays.toString(values));
+    }
 
 }
--- Fundamentals/basic-exercises/src/Main.java
+++ Fundamentals/basic-exercises/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array = {1,5,10,-2};
         BasicJava basicJava = new BasicJava();
+        basicJava.minMaxAndAvg(array);
     }
 }
